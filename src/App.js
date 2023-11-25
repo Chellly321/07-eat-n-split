4564ae62997feba826ae594e5c46d94b103645a1
Chelly321 <michelle.royer@icloud.com>
2023-11-25 09:24:48 +0100

    conditional-form-and-submit-form

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import FriendsList from "./FriendsList";
 import { initialFriends } from "./initialFriends";
 import Button from "./Button";
 import FormSplitBill from "./FormSplitBill";
+import { useState } from "react";
 
 function App() {
   const friends = initialFriends;
+  const [showAddFriend, setShowAddFriend] = useState(false);
 
+  function showForm() {
+    setShowAddFriend((show) => !show);
+  }
   return (
-    <div className="app">
-      <div className="sidebar">
-        <FriendsList friends={friends} />
-        <FormAddFriend friends={friends} />
-        <Button>Add friend</Button>
+    <>
+      <div className="app">
+        <div className="sidebar">
+          <FriendsList friends={friends} />
+          {showAddFriend && <FormAddFriend friends={friends} />}
+          <Button onClick={showForm}>
+            {showAddFriend ? "Close" : "Add friend"}
+          </Button>
+        </div>
+        <FormSplitBill />
       </div>
-      <FormSplitBill />
-    </div>
+    </>
   );
 }
 
--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,9 @@
-function Button({ children }) {
-  return <div className="button">{children}</div>;
+function Button({ children, onClick }) {
+  return (
+    <div className="button" onClick={onClick}>
+      {children}
+    </div>
+  );
 }
 
 export default Button;
